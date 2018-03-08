7e369b168609ed29e945f2794bbe047022a2eeae
Nikita Bolshagin <34246818+bolshagin@users.noreply.github.com>
2018-03-08 14:12:42 +0500

    initial commit

---
+++ Client/Client.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Client
+{
+    internal class Client
+    {
+        private const int Port = 8888;
+        private const string Address = "127.0.0.1";
+
+        private const string StopWord = "/stop";
+
+        private static readonly string[] Separators = {",", ".", "!", "?", ";", ":", " "};
+
+        private static readonly List<string> Words = new List<string>();
+
+        private static void GetWordsFromAllFiles(string path)
+        {
+            try
+            {
+                var files = Directory.GetFiles(path, "*.txt", SearchOption.TopDirectoryOnly);
+                if (files.Length > 0)
+                {
+                    foreach (var file in files)
+                    {
+                        var lines = File.ReadLines(file, Encoding.UTF8);
+                        foreach (var line in lines)
+                        {
+                            var words = line.Split(Separators, StringSplitOptions.RemoveEmptyEntries);
+                            foreach (var word in words) Words.Add(word);
+                        }
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nОтсутствуют *.txt файлы в данном каталоге");
+                }
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void Answer(string data)
+        {
+            var answer = Convert.ToBoolean(data);
+            Console.WriteLine(answer ? "Ответ: палиндром" : "Ответ: не палиндром");
+        }
+
+        private static void SendData(string word, NetworkStream stream)
+        {
+            var data = Encoding.UTF8.GetBytes(word + Environment.NewLine);
+            stream.Write(data, 0, data.Length);
+        }
+
+        private static string ReadResponseData(NetworkStream stream)
+        {
+            var data = new byte[5];
+            var bytes = stream.Read(data, 0, data.Length);
+            var responseData = Encoding.UTF8.GetString(data, 0, bytes);
+            return responseData;
+        }
+
+        private static bool CheckArguments(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Неверное число аргументов");
+                Console.WriteLine(
+                    "Клиент принимает только один параметр - путь до каталога. \nНапример: Client.exe C:\\path\\to\\txt\\files");
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Title = "Клиент";
+
+            if (CheckArguments(args)) return;
+
+            GetWordsFromAllFiles(args[0]);
+
+            try
+            {
+                var client = new TcpClient(Address, Port);
+                using (var stream = client.GetStream())
+                {
+                    foreach (var word in Words)
+                        try
+                        {
+                            Console.WriteLine("\nОтправляю на сервер: {0}", word);
+                            SendData(word, stream);
+
+                            var responseData = ReadResponseData(stream);
+
+                            Answer(responseData);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+
+                    Console.WriteLine("\nEnter, чтобы закрыть клиент");
+                    Console.ReadKey();
+
+                    SendData(StopWord, stream);
+
+                    stream.Close();
+                    client.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    internal class Server
+    {
+        private const int Port = 8888;
+        private const string StopWord = "/stop";
+
+        private static readonly List<Task> Tasks = new List<Task>();
+
+        private static bool IsPalindrome(string str)
+        {
+            var n = str.Length;
+            for (var i = 0; i < n / 2; i++)
+                if (str[i] != str[n - i - 1])
+                    return false;
+            return true;
+        }
+
+        private static void Main(string[] args)
+        {
+            Console.Title = "Сервер";
+
+            var tokenSource = new CancellationTokenSource();
+
+            TcpListener server = null;
+            try
+            {
+                server = new TcpListener(IPAddress.Any, Port);
+                server.Start();
+                Console.WriteLine("Сервер запущен. Ожидание подключения...");
+
+                var task = Listen(server, tokenSource.Token);
+                Console.ReadKey();
+
+                try
+                {
+                    tokenSource.Cancel();
+                    server.Stop();
+                    task.Wait(tokenSource.Token);
+                }
+                catch (ObjectDisposedException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (OperationCanceledException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static async Task Listen(TcpListener server, CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+                try
+                {
+                    var client = await server.AcceptTcpClientAsync();
+                    Console.WriteLine("Подключен клиент.");
+                    Tasks.Add(Process(client, token));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+            await Task.WhenAll(Tasks);
+        }
+
+        private static async Task Process(TcpClient client, CancellationToken token)
+        {
+            var data = new byte[256];
+            var b = new StringBuilder();
+            using (var stream = client.GetStream())
+            {
+                do
+                {
+                    try
+                    {
+                        var bytes = await stream.ReadAsync(data, 0, data.Length, token);
+                        if (bytes > 0)
+                        {
+                            b.Append(Encoding.UTF8.GetString(data, 0, bytes));
+                            var index = b.IndexOf(Environment.NewLine, 0, true);
+                            if (index > 0)
+                            {
+                                var word = b.ToString(0, index);
+                                if (word == StopWord)
+                                {
+                                    client.Close();
+                                    break;
+                                }
+
+                                b.Remove(0, index + Environment.NewLine.Length);
+                                await Task.Delay(1000);
+                                Console.WriteLine("Принято от клиента: {0}", word);
+                                data = Encoding.UTF8.GetBytes(IsPalindrome(word.Trim()).ToString());
+                                stream.Write(data, 0, data.Length);
+                            }
+                        }
+                        else
+                        {
+                            await Task.Delay(1);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                } while (client.Connected && !token.IsCancellationRequested);
+            }
+
+            Console.WriteLine("Клиент отключен");
+        }
+    }
+}
